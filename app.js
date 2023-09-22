08a06b41dbaa28f6ba9c75be93369d4e9cd02fcc
jagemdra <jagendrasrivastava007@gmail.com>
2023-09-22 17:59:54 +0530

    add user registration and login  authentication using passport passport-local-mongoose

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const mongoose = require("mongoose");
 
-const port  = 3000;   
-
-const mongoose  = require("mongoose");
-
+const port = 3000;
 const app = express();
-
-
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -20,90 +17,114 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-//connecting to Database
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-     .then(async ()=>{
-      console.log('Connected to MongoDB Atlas')
-     })
-    .catch(error=>{
-      console.log('Error in Connecting to MongoDB Atlas', error)
+app.use(session({
+    secret: "Jagendra Srivastava",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connecting to Database
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(async () => {
+        console.log('Connected to MongoDB Atlas')
+    })
+    .catch(error => {
+        console.log('Error in Connecting to MongoDB Atlas', error)
     })
 
-// create schema 
-
+// Create schema
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
-    email: String,
+    email: String, // Keep the 'email' field
     password: String
-})
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+// Create model
+const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 
 
-//create model
-
-const User = new mongoose.model('User', userSchema);
-
-
-
-
-
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.render("home");
 })
 
-app.get('/login', (req,res)=>{
+app.get('/login', (req, res) => {
     res.render("login");
 })
 
-app.get('/register', (req,res)=>{
+app.get('/register', (req, res) => {
 
 
     res.render("register");
 })
 
-
-
-
-
-app.post("/register", (req, res)=>{
-
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email:req.body.username,
-            password:hash
-        })
-    
-        newUser.save()
-         .then(()=>{
-            console.log(`User ${newUser.email} is saved`);
-            res.render('secrets');
-         })
-         .catch((error)=>{
-            console.log(`User not saved :`, error);
-         })
-    });
-
+app.get('/secrets', (req,res)=>{
+     if(req.isAuthenticated()){
+        res.render("secrets.ejs");
+     }else{
+        res.redirect("/login");
+     }
    
-
-    // res.redirect('/');
 })
 
-
-app.post("/login",async (req, res)=>{
-
-    const username = req.body.username;
-    const password = req.body.password;
-    
-    const foundUser = await User.findOne({email:username}).exec();
-    bcrypt.compare(password, foundUser.password, function(err, result) {
-        // result == true
-        if(result === true){
-            console.log("User Matched!!!");
-            res.render('secrets');
+app.get("/logout", function(req, res){
+    req.logout(function(err) {
+        if (err) {
+            console.error(err);
         }
+        res.redirect("/");
     });
-   
+});
+
+
+app.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username // Use 'username' from your form for the 'email' field
+    });
+
+    try {
+        await User.register(newUser, req.body.password);
+        // res.redirect("/login");
+        res.redirect("/secrets")
+    } catch (error) {
+        console.error(error);
+        res.redirect("/register"); // Redirect back to the registration page on error
+    }
+});
+
+
+
+app.post("/login", async (req, res) => {
+
+     const user = new User({
+        username: req.body.username,
+        password: req.body.password
+     })
+  
+     req.login(user, function(err) {
+        if (err) {
+             console.log(err) 
+        }else{
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/secrets")
+            } )
+        }
+       
+      });
+     
 
 });
 
@@ -111,6 +132,6 @@ app.post("/login",async (req, res)=>{
 
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(` Server is running on port ${port} Successfully !!!`);
 })
